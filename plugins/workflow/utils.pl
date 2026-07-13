a58bc54282c39f54e6bdfaa1b2324c63ecb5a53b
Vitaly Bogdanov <vsbogd@gmail.com>
2026-07-13 19:13:43 +0300

    Unite workflow plugin files into single MeTTa module
    Because of https://github.com/trueagi-io/PeTTa/issues/196 the only way
    of writing MeTTa plugins now is using a single MeTTa file.

--- plugins/workflow/utils.pl
+++
@@ -1,6 +0,0 @@
-safe_exists_file(Path, true)       :- exists_file(Path), !.
-safe_exists_file(_, false).
-
-
-safe_exists_directory(Path, true)  :- exists_directory(Path), !.
-safe_exists_directory(_, false).
